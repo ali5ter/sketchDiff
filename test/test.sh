e75871d6fa163a89c432b27e51f0ec3b152408c7
Alister Lewis-Bowen <bowena@vmware.com>
2018-04-04 16:11:09 -0400

    refactor for fix of file validation

--- test/test.sh
+++ test/test.sh
@@ -7,18 +7,49 @@ SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 skdiff="$SCRIPTPATH/../skdiff"
 cd "$SCRIPTPATH"
 
+OUTPUT_DIR='test_changes'
+
 [[ "$1" == '-d' ]] && set -x
 
+heading() {
+    echo
+    printf '=%.0s' {1..79}
+    echo -e "\n$1" | fold -s -w 79
+    printf -- '-%.0s' {1..79}
+    echo
+    return 0
+}
+
+cleanup() {
+    rm -fR "$OUTPUT_DIR" 'changes'
+}
+
+heading 'display help'
 skdiff
 
+heading "validate 'foo' & 'bar' exist"
 skdiff foo bar
-skdiff example_1_artboards bar
-skdiff foo example_1_artboards
-skdiff example_1_artboards example_2_artboards
-skdiff example_1.sketch example_1_artboards
 
-skdiff example_1.sketch example_2.sketch \
-    --output test_changes
+heading "validate 'test.sh' is a sketch file & 'bar' exists"
+skdiff test.sh bar
 
+heading "validate 'foo' exists & 'test.sh' is a sketch file"
+skdiff foo test.sh
+
+heading "validate 'test.sh' & 'composite.png' are sketch files"
+skdiff test.sh composite.png
+
+heading "diff summary and output to '$OUTPUT_DIR'"
+cleanup
 skdiff example_1.sketch example_2.sketch \
-    --output test_changes -q
+    --output "$OUTPUT_DIR"
+
+heading "diff summary and output to '$OUTPUT_DIR' with no messages"
+cleanup
+skdiff example_1.sketch example_2.sketch \
+    --output "$OUTPUT_DIR" -q
+
+heading "diff summary only"
+cleanup
+skdiff example_1.sketch example_2.sketch \
+    --no-output
